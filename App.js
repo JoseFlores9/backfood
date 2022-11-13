31f8bbffd2114d4745b3313274bb5e0cc47d934d
Dsaw <jos.flores95@gmail.com>
2022-11-13 16:59:18 -0300

    [ADD] first commit

---
+++ App.js
@@ -0,0 +1,75 @@
+const http = require('http')
+
+http.createServer( (req, res) => {
+    const responseProducts = {
+        products: [
+            {
+                title: "Coca Cola 500 ml",
+                image: "https://www.purochilelluta.cl/wp-content/uploads/2021/01/coca-normal-500.jpg",
+                price: "$ 800",
+                stock: 50,
+                productId: 1
+            },
+            {
+                title: "Coca Cola 500 ml",
+                image: "https://www.purochilelluta.cl/wp-content/uploads/2021/01/coca-normal-500.jpg",
+                price: "$ 800",
+                stock: 50,
+                productId: 2
+            },
+            {
+                title: "Coca Cola 500 ml",
+                image: "https://www.purochilelluta.cl/wp-content/uploads/2021/01/coca-normal-500.jpg",
+                price: "$ 800",
+                stock: 50,
+                productId: 3
+            },
+            {
+                title: "Coca Cola 500 ml",
+                image: "https://www.purochilelluta.cl/wp-content/uploads/2021/01/coca-normal-500.jpg",
+                price: "$ 800",
+                stock: 50,
+                productId: 4
+            },
+            {
+                title: "Coca Cola 500 ml",
+                image: "https://www.purochilelluta.cl/wp-content/uploads/2021/01/coca-normal-500.jpg",
+                price: "$ 800",
+                stock: 50,
+                productId: 5
+            },
+            {
+                title: "Coca Cola 500 ml",
+                image: "https://www.purochilelluta.cl/wp-content/uploads/2021/01/coca-normal-500.jpg",
+                price: "$ 800",
+                stock: 50,
+                productId: 6
+            },
+            {
+                title: "Coca Cola 500 ml",
+                image: "https://www.purochilelluta.cl/wp-content/uploads/2021/01/coca-normal-500.jpg",
+                price: "$ 800",
+                stock: 50,
+                productId: 7
+            },
+            {
+                title: "Coca Cola 500 ml",
+                image: "https://www.purochilelluta.cl/wp-content/uploads/2021/01/coca-normal-500.jpg",
+                price: "$ 800",
+                stock: 50,
+                productId: 8
+            },
+            {
+                title: "Coca Cola 500 ml",
+                image: "https://www.purochilelluta.cl/wp-content/uploads/2021/01/coca-normal-500.jpg",
+                price: "$ 800",
+                stock: 50,
+                productId: 9
+            }
+        ]
+    }
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.write( JSON.stringify(responseProducts))
+    res.end()
+})
+.listen( 8080)
